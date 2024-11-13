e227431025e9942754f99e3b7dfc4b4a347afca1
leonardo vinicius <leonardovinicius9987@gmail.com>
2024-11-12 22:37:49 -0300

    fisrt commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# resistro
+
+A new Flutter project.
---
+++ lib/themes/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor =
+      Color(0xFFFFA500); // Laranja para pastas e botão
+  static const Color textColor = Colors.black;
+  static const Color backgroundColor = Colors.white;
+  static const Color iconColor = Colors.grey;
+}
